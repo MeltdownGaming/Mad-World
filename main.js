eabd2c5da0467b587dd4a117873c9f8f940feac5
Anay Mittal <anayttal@gmail.com>
2021-11-19 17:22:37 -0500

    Going to write custom controls

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Follow Genka Character control video - Watch Stack Overflow for an answer to question.
+//Write Custom controls.
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
